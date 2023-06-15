bbaab07440d6a7bb0f46e8497f2b79d62155df08
2710kng <130636585+2710kng@users.noreply.github.com>
2023-06-15 14:02:37 +0900

    0615

--- fukuv0615/Form1.cs
+++ fukuv0615/Form1.cs
@@ -2,9 +2,24 @@ namespace fukuv0615
 {
     public partial class Form1 : Form
     {
+        int ans = rand.Next(0, 100);
+        int count = rand.Next(0);
+        static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count += 1;
+            MessageBox.Show("(count)‰ñ–Ú‚Ì’§í");
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
